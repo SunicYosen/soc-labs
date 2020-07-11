8f08f55a0a378bd5e9a3b95c11372927faa8b5b7
sunicyosen <sunicyosen@outlook.com>
2020-07-11 22:12:24 +0800

    Add vcs & verdi files

---
+++ lab1/vcs-verdi/file.f
@@ -0,0 +1,12 @@
+../src/rtl/mac.v
+../src/rtl/fifo.v
+../src/rtl/pe3.v
+../src/rtl/pe33.v
+../src/rtl/relu.v
+../src/rtl/conv.v
+../src/rtl/conv_top.v
+../src/tb/conv_top_tb.v
+../src/tb/fifo_tb.v
+../src/tb/mac_tb.v
+../src/tb/pe3_tb.v
+../src/tb/pe33_tb.v
